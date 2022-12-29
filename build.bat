34c4eb3253c8a8987d91c36f370327f3ed45383c
Harvey Dela Cruz <dc.harvs@gmail.com>
2022-12-29 22:09:17 +0800

    Add build script

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+::update and compile deps
+call mix deps.get --only prod
+set MIX_ENV=prod
+call mix compile
+
+::compile assets
+call mix assets.deploy
+
+::cleanup
+set MIX_ENV=dev
+call mix phx.digest.clean --all
